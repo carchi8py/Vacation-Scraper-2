c09ec7cf3718deaae56e5bccc972abb1e0c14b21
Chris Archibald <carchi8py@gmail.com>
2017-08-09 16:08:33 -0700

    Removing dead code:

--- hotel/vegas.py
+++ hotel/vegas.py
@@ -1,23 +1,12 @@
 from bs4 import BeautifulSoup
-import requests
 import datetime
 from selenium import webdriver
-import sys
-
-import urllib
 
 PHANTOMJS_PATH = "/Users/carchi/bin/phantomjs-2.1.1-macosx"
 
 def main():
-    today = datetime.date.today()
-    tomarrow = today + datetime.timedelta(days=1)
-    guests = 2
-
-    #browser = webdriver.PhantomJS(PHANTOMJS_PATH)
     driver = webdriver.Chrome()
-    params = {"arrive": today, "depart": tomarrow, "numGuest": guests}
     driver.get('https://www.mgmgrand.com/en/booking/room-booking.html')
-
 
     soup = BeautifulSoup(driver.page_source, "html.parser")
     results = soup.find("div", {"id": "rate-calendar-months-wrapper"})
@@ -39,11 +28,6 @@ def main():
     my_data.sort(key=lambda x: x["price"])
     for data in my_data:
         print(data["date"] + ": $" + str(data["price"]))
-
-
-
-    print(len(days))
-
 
 if __name__ == "__main__":
     main()
