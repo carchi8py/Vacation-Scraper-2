c4ba998f99b28abd6f84c0b5eaa8b38e67a5f0f8
Chris Archibald <carchi8py@gmail.com>
2017-08-10 16:01:33 -0700

    Adding database

--- hotel/vegas.py
+++ hotel/vegas.py
@@ -1,7 +1,22 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
-import sys
+import sys, os, inspect
 import time
+import datetime
+
+#import the database
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0,parentdir)
+from database_setup import Base, Hotel, Price
+
+engine = create_engine('sqlite:///db.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
 hotels = ["mandalaybay",
           "delanolasvegas",
@@ -20,6 +35,7 @@ hotels = ["mandalaybay",
 def main():
     my_data = []
     for hotel in hotels:
+        add_hotel_to_db(hotel)
         request = get_website(hotel)
         print(hotel)
         days = parse_data(request)
@@ -28,6 +44,17 @@ def main():
     for price in my_data:
         print(price["date"] + ": $" + str(price["price"]) + " at " + price["hotel"])
 
+def add_hotel_to_db(hotel):
+    new_hotel = Hotel(name = hotel)
+    session.add(new_hotel)
+    session.commit()
+
+def add_price_to_db(date, rate, hotel):
+    hotel_obj = session.query(Hotel).filter_by(name=hotel).one()
+    date_obj = datetime.datetime.strptime(date, "%m/%d/%Y").date()
+    new_price = Price(hotel = hotel_obj, date = date_obj, price = rate)
+    session.add(new_price)
+    session.commit()
 
 def parse_data(request):
     soup = BeautifulSoup(request.page_source, "html.parser")
@@ -49,6 +76,7 @@ def get_prices(days, my_data, hotel):
         rate = int(rate.text.split("$")[1])
         date = data_month + "/" + data_date + "/" + data_year
         my_data.append({"date": date, "price": rate, "hotel": hotel})
+        add_price_to_db(date, rate, hotel)
     return my_data
 
 def get_website(hotel):
