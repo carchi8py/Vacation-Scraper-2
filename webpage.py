c562b999b6f221c42658b7c7028e0594902e6c10
Chris Archibald <carchi8py@gmail.com>
2017-08-10 17:08:35 -0700

    Print out cheepest price for a certain month and number of days

--- webpage.py
+++ webpage.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
+import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Hotel, Price
+import sys
 
 app = Flask(__name__)
 
@@ -15,6 +17,27 @@ session = DBSession()
 def index():
     hotels = session.query(Hotel).order_by(Hotel.name)
     return render_template('index.html', hotels=hotels)
+
+@app.route('/cheepest/<month>/<days>')
+def show_cheepest_for_days(month, days):
+    hotels = session.query(Hotel).order_by(Hotel.name)
+    data = []
+    for hotel in hotels:
+        low_date = 0
+        high_date = int(days)
+        #this get us all the price for a specific month
+        month_prices = session.query(Price).filter_by(hotel = hotel).filter((sqlalchemy.extract('month', Price.date) == month)).order_by(Price.date)
+        while month_prices.count() > high_date:
+            cost = 0
+            for price in range(low_date, high_date):
+                cost += month_prices[price].price
+            data.append({"start": str(month_prices[low_date].date), "end": str(month_prices[high_date].date), "price": int(cost), "hotel": month_prices[high_date].hotel.name})
+            low_date += 1
+            high_date += 1
+    data.sort(key=lambda x: x["price"])
+    for each in data:
+        print(each["start"] + " to " + each["end"]+ ": $" + str(each["price"]) + " at " + each["hotel"])
+
 
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
