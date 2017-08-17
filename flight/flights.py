e886f6adce34050fc8fb45d7d048095da6c2ec39
Chris Archibald <carchi8py@gmail.com>
2017-08-16 18:35:48 -0700

    add flight in both directions

--- flight/flights.py
+++ flight/flights.py
@@ -5,6 +5,7 @@ much what i want.
 """
 
 import datetime
+from datetime import timedelta
 import calendar
 import requests
 import json
@@ -41,9 +42,16 @@ def main():
     now = datetime.datetime.now()
     my_data = []
     for airport in AIRPORTS:
-        print("Flight for " + airport)
-        url = generate_url(now, airport)
-        r = requests.get(url)
+        print("Flight from " + airport + " to " + VEGAS)
+        to_url = generate_url(now, airport, VEGAS)
+        r = requests.get(to_url)
+        parsed_json = json.loads(r.text)
+        flights = parsed_json["data"]
+        my_data = parse_flights(flights, my_data)
+
+        print("Flight from  " + VEGAS + " to " + airport)
+        to_url = generate_url(now, VEGAS, airport)
+        r = requests.get(to_url)
         parsed_json = json.loads(r.text)
         flights = parsed_json["data"]
         my_data = parse_flights(flights, my_data)
@@ -79,9 +87,10 @@ def add_flight_to_db(start, end, departure, arrival, price, link, airline):
     session.commit()
 
 
-def generate_url(date_obj, airport):
-    url = URL1 + airport + URL2 + VEGAS + URL3
+def generate_url(date_obj, from_airport, to_airport):
+    url = URL1 + from_airport + URL2 + to_airport + URL3
     url = url + date_obj.strftime("%d/%m/%Y") + URL4
+    date_obj = date_obj + timedelta(days=365)
     last_day_of_month = str(calendar.monthrange(date_obj.year, date_obj.month)[1])
     url = url + last_day_of_month + date_obj.strftime("/%m/%Y") + URL5
     return url
